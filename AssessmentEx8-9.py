fe6e1295f2ba13c4aed1de6223351d990340e831
Guilhermesav <54535484+Guilhermesav@users.noreply.github.com>
2019-12-11 07:45:45 -0300

    Update AssessmentEx8-9.py

--- AssessmentEx8-9.py
+++ AssessmentEx8-9.py
@@ -27,12 +27,15 @@ if __name__ == "__main__":
     entradas = [1,5,2,3,7,2,9,8]
     print('Sequencial')
     saidas = []
+    t_seq_ini = float(time.time())
     for i in entradas:
         resultado = fatorial(i)
         saidas.append(resultado)
     print(saidas)
+    t_seq_fim = float(time.time())
     N = len(entradas)
     print('Threading')
+    t_th_ini = float(time.time())
     saidas_t = []
     Nthreads = 4
     lista_threads =[]
@@ -46,8 +49,11 @@ if __name__ == "__main__":
         
     for t in lista_threads:
         t.join()
-    
+
+    t_th_fim = float(time.time())
+
     print('Multiprocessing')
+    t_mp_ini = float(time.time())
     NProc = 4
     
     q_entrada = multiprocessing.Queue()
@@ -67,7 +73,9 @@ if __name__ == "__main__":
     for p in lista_proc:
         p.join() 
     
+    t_mp_fim = float(time.time())
     
-    
-    
+    print("Tempo Sequencial:",(t_seq_fim - t_seq_ini))
+    print("Tempo com threading:",(t_th_fim - t_th_ini))
+    print("Tempo com multiprocessing:",(t_mp_fim - t_mp_ini))
     
