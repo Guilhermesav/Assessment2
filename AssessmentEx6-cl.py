bdb5d187fa30a68ee98d5759b9befbcca2ff7729
Guilhermesav <54535484+Guilhermesav@users.noreply.github.com>
2019-12-11 08:06:23 -0300

    Update AssessmentEx6-cl.py

--- AssessmentEx6-cl.py
+++ AssessmentEx6-cl.py
@@ -18,3 +18,31 @@ except Exception as erro:
     print(str(erro))
 s.close()
 input("Pressione qualquer tecla para sair...")
+
+"""import socket
+import psutil, pickle
+import os
+
+
+socket_servidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+
+
+host = socket.gethostname()
+porta = 8881
+socket_servidor.bind((host, porta))
+socket_servidor.listen()
+print("Servidor de nome", host, "esperando conexão na porta", porta)
+
+while True:
+(socket_cliente,addr) = socket_servidor.accept()
+print("Conectado a:", str(addr))
+msg = socket_cliente.recv(1024)
+nome_dir = msg.decode('ascii')
+if os.path.isdir(nome_dir):
+arquivos = os.listdir(nome_dir) socket_cliente.send(arquivos.encode('ascii'))
+else:
+print("Não encontrou o diretorio")
+socket_cliente.send('-1'.encode('ascii'))
+
+socket_cliente.close()"""
